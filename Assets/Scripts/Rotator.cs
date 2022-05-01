656db6523278e87cae60153f62be48c0305eaa54
Nariman <narik454@yandex.ru>
2022-05-01 19:03:22 +0300

    first commit

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float speed;
+    private Transform _rotator;
+
+    void Start()
+    {
+        _rotator = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        _rotator.Rotate(0, speed * Time.deltaTime, 0);
+    }
+}
